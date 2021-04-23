1bb2bad6c90901e538048e0237e157ad4dff9c13
KhairulIslamAzam <khairulisla.azam@gmail.com>
2021-04-23 15:22:45 +0600

    added kafka consumer basic properties

---
+++ spring-boot-with-kafka-consumer/src/main/java/com/example/kafka/SpringBootWithKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithKafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithKafkaConsumerApplication.class, args);
+    }
+
+}
